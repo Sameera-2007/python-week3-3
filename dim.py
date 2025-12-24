8a14c2a9df56438dbf2c7c3c96a583c5b491f614
Sameera-2007 <24331a05f7@mvgrce.edu.in>
2025-12-24 19:58:39 +0530

    Add files via upload

---
+++ dim.py
@@ -0,0 +1,9 @@
+import numpy as np
+x=np.array([1,2,3,4,5])
+print(x)
+print("dimension:",x.ndim)
+print("size:",x.size)
+y=np.array([[1,2,3],[4,5,6]])
+print(y)
+print("dimension:",y.ndim)
+print("size:",y.size)
